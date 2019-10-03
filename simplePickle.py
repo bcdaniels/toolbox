bfba7a78058ff7bdf0bd1a33abf1e93e6bceb018
Bryan Daniels <bryan.daniels.1@asu.edu>
2019-10-03 13:57:29 +0200

    update simplePickle to Python 3

--- simplePickle.py
+++ simplePickle.py
@@ -5,15 +5,23 @@
 #
 
 import io
-import cPickle
+import sys
+
+if sys.version_info.major > 2:
+    # Python 3
+    import pickle
+else:
+    # Python 2
+    import cPickle as pickle
 
 def save(obj,filename):
     fout = io.open(filename,'wb')
-    cPickle.dump(obj,fout,2)
+    # Note: we currently save using backward-compatible protocol 2
+    pickle.dump(obj,fout,2)
     fout.close()
 
 def load(filename):
     fin = io.open(filename,'rb')
-    obj = cPickle.load(fin)
+    obj = pickle.load(fin)
     fin.close()
     return obj
